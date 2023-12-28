3b5ed042f3014bf7ae32de338a392ef1ba015b28
DeRon Sutton <dsutton1080@gmail.com>
2023-12-28 16:35:44 -0600

    implemented CRUD operations for skillsRoutes

--- routes/skillsRoutes.js
+++ routes/skillsRoutes.js
@@ -3,8 +3,39 @@ const router = express.Router();
 const prisma = require("@prisma/client");
 
 router.get("/skills/all", async (_request, response) => {
-  const skills = await prisma.skill.findMany();
+  const skills = await prisma.skills.findMany();
   response.send(skills);
+});
+
+router.post("/skills", async (request, response) => {
+  const newSkill = await prisma.skills.create({
+    data: request.body,
+    orderBy: {
+      order: "desc",
+    },
+  });
+  response.send(newSkill);
+});
+
+router.patch("/skills/:id", async (request, response) => {
+  const { id } = request.params;
+  const skill = await prisma.skills.update({
+    where: {
+      id: parseInt(id),
+    },
+    data: request.body,
+  });
+  response.send(skill);
+});
+
+router.delete("/skills/:id", async (request, response) => {
+  const { id } = request.params;
+  const skill = await prisma.skills.delete({
+    where: {
+      id: parseInt(id),
+    },
+  });
+  response.send(skill);
 });
 
 module.exports = router;
