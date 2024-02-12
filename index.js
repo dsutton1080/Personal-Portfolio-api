dd27880c82573ad5a5a3cb92ff0c0b10161ea4a1
DeRon Sutton <dsutton1080@gmail.com>
2024-02-11 20:05:36 -0600

    Add dbFileRoutes via google drive api,

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ const sectionRoutes = require("./routes/sectionRoutes");
 const contentRoutes = require("./routes/contentRoutes");
 const experienceRoutes = require("./routes/experienceRoutes");
 const projectRoutes = require("./routes/projectRoutes");
+const dbFileRoutes = require("./routes/dbFileRoutes");
 
 app.use(cors()); // This will set the Access-Control-Allow-Origin header to *
 app.use(express.json());
@@ -17,9 +18,16 @@ app.use("/section", sectionRoutes);
 app.use("/content", contentRoutes);
 app.use("/experience", experienceRoutes);
 app.use("/project", projectRoutes);
+app.use("/dbfile", dbFileRoutes.router);
 
 app.listen(PORT, () => {
 	console.log(`Server running on port ${PORT}`);
+	dbFileRoutes.getDbFile();
+
+	const interval = 8 * 60 * 60 * 1000; // 8 hours
+	setInterval(() => {
+		dbFileRoutes.updateDbFile();
+	}, interval);
 });
 
 app.get("/status", (_request, response) => {
@@ -28,4 +36,11 @@ app.get("/status", (_request, response) => {
 		timestamp: new Date(),
 	};
 	response.send(status);
+});
+
+process.on("SIGINT", async function () {
+	console.log("Closing down server.");
+	await dbFileRoutes.updateDbFile().then(() => {
+		process.exit();
+	});
 });
